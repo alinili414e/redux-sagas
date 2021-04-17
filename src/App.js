83ec1badf9018696710909e3e20ac6a4fd7a4e2a
Ali Nili <alinili414e@gmail.com>
2021-04-17 12:33:33 -0400

    Adding Income and Expense

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
+
+import { Container, Grid, GridColumn, Header, Segment, Statistic } from 'semantic-ui-react';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Header as="h1">Budget</Header>
+      <Statistic size="small">
+        <Statistic.Label>Your Balance:</Statistic.Label>
+        <Statistic.Value>2,550.53</Statistic.Value>
+      </Statistic>
+      <Segment textAlign="center">
+        <Grid columns={2} divided>
+          <Grid.Row>
+            <GridColumn>
+              <Statistic size="tiny" color="green">
+                <Statistic.Label style={{ textAlign: 'left' }}>Incoming:</Statistic.Label>
+                <Statistic.Value >1045.50</Statistic.Value>
+              </Statistic>
+            </GridColumn>
+            <GridColumn>
+              <Statistic size="tiny" color="red">
+                <Statistic.Label style={{ textAlign: 'left' }}>Expenses</Statistic.Label>
+                <Statistic.Value >623.50</Statistic.Value>
+              </Statistic>
+            </GridColumn>
+          </Grid.Row>
+        </Grid>
+      </Segment>
+    </Container>
   );
 }
-
 export default App;

0bef8248f162dc566f5a3cf76b2d706cf3c10c77
Ali Nili <alinili414e@gmail.com>
2021-04-18 11:35:08 -0400

    Created Entryline Component

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import DisplayBalance from './components/DisplayBalance';
 import MainHeader from './components/MainHeader';
 import NewEntryForm from './components/NewEntryForm';
 import DisplayBalances from './components/DisplayBalances';
+import EntryLines from './components/EntryLines';
 function App() {
   return (
     <Container>
@@ -12,42 +13,8 @@ function App() {
       <MainHeader title="Budget" />
       <DisplayBalance size='small' title="Your Balance:" value="2,550.53" />
       <MainHeader type="h3" title="History" />
-      <Segment color="red">
-        <Grid columns={3} textAlign="right">
-          <Grid.Row>
-            <Grid.Column width={10} textAlign="left">Something</Grid.Column>
-            <Grid.Column width={3} textAlign="right">$10,000</Grid.Column>
-            <Grid.Column width={3}>
-              <Icon name="edit" bordered />
-              <Icon name="trash" bordered />
-            </Grid.Column>
-          </Grid.Row>
-        </Grid>
-      </Segment>
-      <Segment color="green">
-        <Grid columns={3} textAlign="right">
-          <Grid.Row>
-            <Grid.Column width={10} textAlign="left">Something Else</Grid.Column>
-            <Grid.Column width={3} textAlign="right">$100,000</Grid.Column>
-            <Grid.Column width={3}>
-              <Icon name="edit" bordered />
-              <Icon name="trash" bordered />
-            </Grid.Column>
-          </Grid.Row>
-        </Grid>
-      </Segment>
-      <Segment color="red">
-        <Grid columns={3} textAlign="right">
-          <Grid.Row>
-            <Grid.Column width={10} textAlign="left">Something</Grid.Column>
-            <Grid.Column width={3} textAlign="right">$20,000</Grid.Column>
-            <Grid.Column width={3}>
-              <Icon name="edit" bordered />
-              <Icon name="trash" bordered />
-            </Grid.Column>
-          </Grid.Row>
-        </Grid>
-      </Segment>
+      <EntryLines desc="Income" value="$10,000" />
+      <EntryLines desc="Expense" value="$10,000" isExpense />
       <MainHeader type="h3" title="Add new Transaction" />
       <NewEntryForm />
     </Container>
